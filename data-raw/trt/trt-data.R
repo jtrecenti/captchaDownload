f6d701153ad731407fa338742f0b84785415115e
jtrecenti <julio.trecenti@gmail.com>
2022-03-15 15:21:11 -0300

    modelo com feedback

--- data-raw/trt/trt-data.R
+++ data-raw/trt/trt-data.R
@@ -3,14 +3,19 @@
 # classificar <- fs::dir_ls("data-raw/trt/img", regexp = "_", invert = TRUE)
 # captcha::classify(classificar, rm_old = TRUE)
 
-tail(readr::read_csv(fs::path_ext_set(path_model, ".log"), show_col_types = FALSE))
 
 
-path_model <- "/home/jt/Documents/jtrecenti/captcha/data-raw/trt.pt"
-modelo <- luz::luz_load(path_model)
+# remotes::install_github("jtrecenti/captchaDownload")
+library(magrittr)
+# tail(readr::read_csv(fs::path_ext_set(path_model, ".log"), show_col_types = FALSE))
+
+
+path_model <- "/home/jt/Documents/jtrecenti/captcha/data-raw/trt_99.pt"
+path_img <- "data-raw/trt/test"
+
+(modelo <- luz::luz_load(path_model))
 
 purrr::rerun(
-  .n = 1,
-  captcha_oracle_trt_com_feedback("data-raw/trt/img_oracle/", modelo, 5)
+  .n = 2,
+  captcha_oracle_trt_com_feedback(path_img, modelo, 50)
 )
-
