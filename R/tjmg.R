158f010326eaa660a47ccc3e7a99565f7ebe5590
Julio Trecenti <julio.trecenti@gmail.com>
2022-05-31 12:30:20 -0300

    add ssl

--- R/tjmg.R
+++ R/tjmg.R
@@ -75,6 +75,7 @@ captcha_download_tjmg <- function(path) {
 captcha_access_tjmg <- function(path) {
   id <- "00666632620118130480"
   u <- "https://www4.tjmg.jus.br/juridico/sf/proc_resultado.jsp?"
+  ssl <- httr::config(ssl_verifypeer = FALSE)
   f_captcha <- fs::file_temp(tmp_dir = path, ext = ".jpeg", pattern = "tjmg")
   httr::handle_reset(u)
   q <- list(
@@ -83,7 +84,7 @@ captcha_access_tjmg <- function(path) {
     listaProcessos = id,
     btn_pesquisar = "Pesquisar"
   )
-  r0 <- httr::GET(u, query = q)
+  r0 <- httr::GET(u, query = q, ssl)
 
   captcha <- r0 |>
     httr::content("text", encoding = "latin1") |>
@@ -98,7 +99,8 @@ captcha_access_tjmg <- function(path) {
 
   httr::GET(
     paste0("https://www4.tjmg.jus.br/juridico/sf/", captcha),
-    httr::write_disk(f_captcha, overwrite = TRUE)
+    httr::write_disk(f_captcha, overwrite = TRUE),
+    ssl
   )
 
   list(f_captcha = f_captcha)
@@ -107,6 +109,7 @@ captcha_access_tjmg <- function(path) {
 
 captcha_test_tjmg <- function(obj, label) {
 
+  ssl <- httr::config(ssl_verifypeer = FALSE)
   payload <- list(
     "callCount" = "1",
     "nextReverseAjaxIndex" = "0",
@@ -120,7 +123,7 @@ captcha_test_tjmg <- function(obj, label) {
     "scriptSessionId" = ""
   )
   u_captcha <- "https://www4.tjmg.jus.br/juridico/sf/dwr/call/plaincall/ValidacaoCaptchaAction.isCaptchaValid.dwr"
-  r_captcha <- httr::POST(u_captcha, body = payload, encode = "form")
+  r_captcha <- httr::POST(u_captcha, body = payload, encode = "form", ssl)
 
   acertou <- httr::content(r_captcha, "text", encoding = "UTF-8") |>
     stringr::str_detect(",true\\)")
